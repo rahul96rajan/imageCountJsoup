551effbf030055b3c99a8193c0f627b2b0b2eb7a
rahul96rajan <rahulrajan@qainfotech.com>
2018-09-03 17:38:24 +0530

    performing the problem on qainfotch.com and sublinks

---
+++ src/main/java/com/assignments/imageCountJsoup/ImageCount.java
@@ -0,0 +1,32 @@
+package com.assignments.imageCountJsoup;
+
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+public class ImageCount {
+	public static void main(String[] args) throws IOException {
+		int gen = 3;
+		String url = "http://www.qainfotech.com";
+		Document doc;
+		Elements allLinks;
+		Elements allImages;
+		doc = Jsoup.connect(url).get();
+
+		for (int i = 0; i < gen; i++) {
+			allLinks = doc.select("a[href]");
+			allImages = doc.getElementsByTag("img");
+			System.out.println("Number of Links in Gen " + (i+1) + " --> " + allLinks.size());
+			System.out.println("Number of Images in Gen " + (i+1) + " --> " + allImages.size());
+			if (!allLinks.get(24).attr("href").contains("http")) {
+				System.out.println("Selecting Sublink --> " + url + allLinks.get(24).attr("href") +"\n");
+				doc = Jsoup.connect(url + allLinks.get(24).attr("href")).get();
+			} else {
+				System.out.println("Selecting Sublink --> " + allLinks.get(24).attr("href") +"\n");
+				doc = Jsoup.connect(allLinks.get(24).attr("href")).get();
+			}
+		}
+
+	}
+}
